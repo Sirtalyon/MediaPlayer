aa9590fccd285bfae938ff83f234e26e43742845
DarkXelus <DarkXelus@users.noreply.github.com>
2018-11-28 16:57:16 +0100

    add genre

--- MediaPlayer/src/Form/MediaType.php
+++ MediaPlayer/src/Form/MediaType.php
@@ -6,7 +6,6 @@ use App\Entity\Genre;
 use App\Entity\Media;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
